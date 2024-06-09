8aa6aca49e1981f51a7cd687460608b55e1bb0bb
TakanariShimbo <takanari.shimbo@gmail.com>
2024-06-09 15:26:52 +0900

    modify

--- ui/home/optimizer.py
+++ ui/home/optimizer.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from optimization import Optimizer, Consts, Table
 
 
@@ -16,4 +18,15 @@ def solve_table(table: Table) -> Table:
     optimizer = Optimizer(consts=consts)
     result_table = optimizer.run()
     assert type(result_table) == Table
+    return result_table
+
+
+def prepare_init_table(n_empty_cells: int, seed: int) -> Table:
+    empty_number_array = np.zeros((9, 9), dtype=int)
+    empty_table = Table(number_array=empty_number_array)
+    consts = Consts(fixed_table=empty_table)
+    optimizer = Optimizer(consts=consts, seed=seed)
+    result_table = optimizer.run()
+    assert type(result_table) == Table
+    result_table.convert_some_cells_to_zero(n_cells_to_zero=n_empty_cells, seed=seed)
     return result_table
